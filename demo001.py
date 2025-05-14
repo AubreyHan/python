296e040723e0b8325d8842b1496f21749fcd3ec7
Aubrey Han MAC <aubreyhan@live.com>
2025-05-14 21:01:15 +0800

    modified:   demo001.py

--- demo001.py
+++ demo001.py
@@ -1,3 +1,18 @@
-a = 123
-b = a
-a = 456
+import asyncio
+import time
+
+async def async_hello_world():
+    now = time.time()
+    await asyncio.sleep(1)
+    print(time.time() - now)
+    print("Hello, world1!")
+    await asyncio.sleep(1)
+    print("Hello, world2!")
+    print(time.time() - now)
+
+async def main():
+    await asyncio.gather(async_hello_world(), async_hello_world(), async_hello_world())
+
+now = time.time()
+# run 3 async_hello_world() coroutine concurrently
+asyncio.run(main())
